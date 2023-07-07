889bcbf80895be6d291693210eb648bc48b7ec62
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-07-07 20:00:38 +0530

    add vercel config

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const app = express();
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("/", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
+}
+
 app.get("/ping", (req, res) => {
   res.json({ msg: "Pong" });
 });
