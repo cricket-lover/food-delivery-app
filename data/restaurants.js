373a232ddba20072f6612b8fdfa49a3864c3e96e
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-07-25 20:09:53 +0530

    reduce image sizes

--- data/restaurants.js
+++ data/restaurants.js
@@ -14,14 +14,14 @@ const defaultRestaurants = [
     name: "Subway",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.1,
   },
   {
     name: "Pizza Hut",
     id: generateRandomId(),
     image:
-      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.5,
   },
   {
@@ -35,28 +35,28 @@ const defaultRestaurants = [
     name: "McDonalds",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.0,
   },
   {
     name: "KFC",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 3.9,
   },
   {
     name: "Subway",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.1,
   },
   {
     name: "Pizza Hut",
     id: generateRandomId(),
     image:
-      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.5,
   },
   {
@@ -70,28 +70,28 @@ const defaultRestaurants = [
     name: "McDonalds",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.0,
   },
   {
     name: "KFC",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 3.9,
   },
   {
     name: "Subway",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.1,
   },
   {
     name: "Pizza Hut",
     id: generateRandomId(),
     image:
-      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.5,
   },
   {
@@ -105,28 +105,28 @@ const defaultRestaurants = [
     name: "McDonalds",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.0,
   },
   {
     name: "KFC",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 3.9,
   },
   {
     name: "Subway",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.1,
   },
   {
     name: "Pizza Hut",
     id: generateRandomId(),
     image:
-      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.5,
   },
   {
@@ -140,28 +140,28 @@ const defaultRestaurants = [
     name: "McDonalds",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.0,
   },
   {
     name: "KFC",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 3.9,
   },
   {
     name: "Subway",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.1,
   },
   {
     name: "Pizza Hut",
     id: generateRandomId(),
     image:
-      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.5,
   },
   {
@@ -175,28 +175,28 @@ const defaultRestaurants = [
     name: "McDonalds",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.0,
   },
   {
     name: "KFC",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 3.9,
   },
   {
     name: "Subway",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      "https://images.unsplash.com/photo-1525980978611-a89001322e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.1,
   },
   {
     name: "Pizza Hut",
     id: generateRandomId(),
     image:
-      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      "https://plus.unsplash.com/premium_photo-1675103908581-521860726818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.5,
   },
   {
@@ -210,14 +210,14 @@ const defaultRestaurants = [
     name: "McDonalds",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 4.0,
   },
   {
     name: "KFC",
     id: generateRandomId(),
     image:
-      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+      "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=280&q=80",
     rating: 3.9,
   },
 ];
