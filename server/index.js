3cac4aee5ff94e5f8d2cc08d85c0355c537ebabe
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-08-25 12:00:42 +0530

    remove production build code

--- server/index.js
+++ server/index.js
@@ -34,19 +34,4 @@ app.delete("/api/logout", authenticateToken, logoutHandler);
 
 app.all("/api/*", pageNotFoundHandler);
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
-  app.get("/*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "build", "index.html"),
-      function (err) {
-        if (err) {
-          res.status(500).json(err);
-        }
-      }
-    );
-  });
-}
-
 app.listen(PORT, () => console.log("Listening on Port", PORT));
